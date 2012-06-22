fa202dcbc66af10a28cc2e474a7086dadb35d6fc
Evan Donovan <evan@evandonovan.org>
2012-06-22 19:20:07 -0400

    Fixing term list function.

--- template.php
+++ template.php
@@ -47,16 +47,16 @@ function ios_jqmobile_preprocess_page(&$variables) {
 function ios_jqmobile_preprocess_node(&$variables) {
  // Creates an imploded list of taxonomy terms for display on the node.
  $node = $variables['node'];
- print_r($node);
+ print_r($content);
  $term_values = array();
  foreach(_ios_jqmobile_term_fields() as $field) {
   $items = field_get_items('node', $node, $field);
   foreach($items as $delta => $term) {
-    $term_values[$field][] = field_view_value('node', $node, $field, $item[$delta]);
+    $term_values[$field][] = render(field_view_value('node', $node, $field, $item[$delta]));
   }
   $term_values[$field] = implode(',', $term_values[$field]);
  }
- $variables['term_list'] = implode($term_values);
+ $variables['term_list'] = implode(',', $term_values);
  // @todo: Add variables as needed.
 }
 
