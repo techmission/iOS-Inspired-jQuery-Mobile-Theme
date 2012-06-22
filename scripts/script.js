9d8d74a2be478406fc0b296eca24f63f7867a1a8
Evan Donovan <evan@evandonovan.org>
2012-06-22 17:39:50 -0400

    Wrapped script in closure.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-// all dialog buttons should close their parent dialog
-$(".ui-dialog button").live("click", function() {		
-  $("[data-role='dialog']").dialog("close");		
-});
+// All dialog buttons should close their parent dialog.
+// Wrapped in a closure so that the code is compatible with Drupal.
+// @see http://drupal.org/node/171213
+(function ($) {
+  $(".ui-dialog button").live("click", function() {		
+    $("[data-role='dialog']").dialog("close");		
+  });
+})(jQuery);
