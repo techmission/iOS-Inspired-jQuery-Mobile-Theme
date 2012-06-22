4577285b446f5ac34a6679bd873d1fbc9dd2646a
Evan Donovan <evan@evandonovan.org>
2012-06-22 17:56:50 -0400

    Added the head variable.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -44,19 +44,13 @@
 ?><!DOCTYPE html> 
 <html> 
 	<head>
-	<!-- TODO: add $head and set these meta tags in template.php --> 
+	<!-- TODO: add $head and set these meta tags in template.php -->
+	<?php print $head; ?> 
 	<meta charset="utf-8" /> 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"> 	 
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<title><?php print $head_title; ?></title>
-	<!-- TODO: switch to using $styles in .info and template.php, once removed broken Drupal styles -->
-	<!--  <link rel="stylesheet"  href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" />-->
-	<!--  <link rel="stylesheet" href="../styles/style.css" />-->
 	<?php print $styles; ?>
-	<!-- TODO: switch to using $scripts in template.php, once removed broken Drupal scripts -->
-	<!--  <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>-->
-	<!-- <script src="../scripts/script.js"></script>-->
 	<?php print $scripts; ?>
 </head> 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
