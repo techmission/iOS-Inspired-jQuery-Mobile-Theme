38170e2530b6f5a91de3c16d42562f9ab5319400
Evan Donovan <evan@evandonovan.org>
2012-06-22 19:25:42 -0400

    Fixing references.

--- template.php
+++ template.php
@@ -47,11 +47,14 @@ function ios_jqmobile_preprocess_page(&$variables) {
 function ios_jqmobile_preprocess_node(&$variables) {
  // Creates an imploded list of taxonomy terms for display on the node.
  $node = $variables['node'];
- print_r($variables['content']);
+ //print_r($variables['content']);
  $term_values = array();
  foreach(_ios_jqmobile_term_fields() as $field) {
+  // Hide from showing as part of the content.
+  hide($content[$field]);
+  // Get the data from the field.
   $items = field_get_items('node', $node, $field);
-  foreach($items as $delta => $term) {
+  foreach($items as $delta => $item) {
     $term_value = field_view_value('node', $node, $field, $item[$delta]);
     print_r($term_value);
     $term_values[$field][] = render($term_value);
